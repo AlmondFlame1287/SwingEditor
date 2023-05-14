de25cf9ceeff78259edb818b43a9b51085c1f24e
Michele Pagano <mpagano284@gmail.com>
2023-05-14 10:51:41 +0200

    Created BlockList class

--- src/main/java/utils/BlocksList.java
+++ src/main/java/utils/BlocksList.java
@@ -1,24 +1,42 @@
 package utils;
 
-import blocks.Block;
+import blocks.*;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
+import java.util.HashSet;
 
 public final class BlocksList {
     private static final Set<Block> blockSet = new HashSet<>();
+    private static Block[] blocks;
+    private static String[] blockNames;
 
-    public static void addBlock(Block block) {
-        blockSet.add(block);
+    public static void initList() {
+        blockSet.add(new GrassBlock());
+        blockSet.add(new Air());
+        blockSet.add(new DirtBlock());
+        blockSet.add(new SpawnPointBlock());
+
+        blocks = new Block[blockSet.size()];
+        blockNames = new String[blocks.length];
+    }
+
+    public static Block[] getBlocks() {
+        if(blocks[0] != null) return blocks;
+
+        int i = 0;
+        for(Block b : blockSet) {
+            blocks[i] = b;
+            i++;
+        }
+        return blocks;
     }
 
     public static String[] getBlockNames() {
-        List<String> blockNames = new ArrayList<>();
-        for(Block b : blockSet) {
-            blockNames.add(b.getName());
+        if(blockNames[0] != null) return blockNames;
+
+        for (int i = 0; i < blockNames.length; i++) {
+            blockNames[i] = blocks[i].getName();
         }
-        return (String[]) blockNames.toArray();
+        return blockNames;
     }
 }
