965fcaa05d98e58dc5f91cdf43367ac84ab19bf9
Michele Pagano <mpagano284@gmail.com>
2023-11-05 22:56:57 +0100

    Changes made to: - `Block(String, int, int, int)`: Method will be deleted in the next commit.
    Added:
    - `loadImage()`

--- src/main/java/blocks/Block.java
+++ src/main/java/blocks/Block.java
@@ -1,22 +1,49 @@
 package blocks;
 
-import java.awt.Color;
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.Serializable;
 
 public class Block implements Serializable {
     public static final int SIZE = 32;
+    public static final String IMAGE_DIR = System.getProperty("user.home") + "/blocks/";
     private final int[] coords = new int[2];
     protected String name;
     protected Color color;
+    protected Image blockTexture;
+
+    public Block(String name) {
+        this.name = name;
+        this.loadImage();
+    }
+
+    private void loadImage() {
+        try {
+            File imgFile = new File(IMAGE_DIR + this.name + ".png");
+            if(!imgFile.exists())
+                throw new FileNotFoundException("File doesn't exist");
+            this.blockTexture = ImageIO.read(imgFile);
+        } catch(FileNotFoundException fnf) {
+            System.err.println("[BLOCK] " + fnf.getMessage());
+        } catch(IOException ioe) {
+            System.err.println("[BLOCK] There was a problem reading the image: " + ioe.getMessage());
+        }
+    }
+
     public Block(String name, Color color) {
         this.name = name;
         this.color = color;
     }
 
+    @Deprecated
     public Block(String name, int r, int g, int b) {
         this.name = name;
         this.color = new Color(r, g, b);
     }
+
 
     public Color getColor() {
         return this.color;
